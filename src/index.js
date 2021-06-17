d232006de9fc846d8953fb2c6eda27b1a4900e05
riyaraa <rinosatyaputra.id@gmail.com>
2021-06-17 12:33:07 +0700

    production

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
 ReactDOM.render(
 	<React.StrictMode>
-		<Router>
+		<Router basename="/">
 			<App />
 		</Router>
 	</React.StrictMode>,
