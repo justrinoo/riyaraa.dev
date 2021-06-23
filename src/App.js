e295fe493d245b595e2496df1b944d14f5d18c1b
Rino Satya Putra <rinosatyaputra.id@gmail.com>
2021-06-23 17:39:52 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import TheApp from "./router/TheApp";
 import "./App.scss";
 function App() {
+	window.document.title = "Rino Satya Putra";
 	return (
 		<div className="App">
 			<TheApp />
