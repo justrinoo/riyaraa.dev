7760ee3375b19290445a7c53c7cab372651f3858
riyaraa <rinosatyaputra.id@gmail.com>
2021-09-02 09:40:01 +0700

    test :smile:

--- src/components/Hero/Blog.jsx
+++ src/components/Hero/Blog.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import Content from "../Blog/Content";
-import Header from "../Blog/Header";
-import Footer from "../Blog/Footer";
+// import Content from "../Blog/Content";
+// import Header from "../Blog/Header";
+// import Footer from "../Blog/Footer";
 
 export default function Blog(props) {
-	const history = props.history;
+	// const history = props.history;
 	return (
 		<>
-			<Header history={history} />
-			<Content history={history} />
-			<Footer />
+			{/* <Header history={history} /> */}
+			{/* <Content history={history} /> */}
+			{/* <Footer /> */}
 		</>
 	);
 }
